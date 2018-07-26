a91a0ae919af12f7d4b1c2612be8d852056adb3b
Alois Napitalai <anapitalai@gmail.com>
2018-07-26 13:31:23 +1000

    still form error

--- static/js/script.js
+++ static/js/script.js
@@ -136,10 +136,10 @@
   $("#contact").submit(function(e) {
   e.preventDefault();
 
- // var $form = $(this);
-  //$.post($form.attr("action"), $form.serialize()).then(function() {
-  //  alert("Thank you!");
- // });
+  var $form = $(this);
+  $.post($form.attr("action"), $form.serialize()).then(function() {
+    alert("Thank you!");
+  });
 });
 
 
